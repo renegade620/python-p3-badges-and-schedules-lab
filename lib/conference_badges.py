62003da90b9dbf0e4ff5934c269ce9b269e9579e
Frankline Were <franklinegift@gmail.com>
2024-09-03 11:59:30 +0300

    loops and comprehensions

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,27 @@
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        list_badges = f"Hello, my name is {name}."
+        badges.append(list_badges)
+    return badges
 
 def assign_rooms(names):
-    return None
+    room_assignments = []
+    for i, name in enumerate(names, 1):
+        room = f"Hello, {name}! You'll be assigned to room {i}!"
+        room_assignments.append(room)
+    return room_assignments
 
 def printer(names):
+    badges = batch_badge_creator(names)
+    room_assignments = assign_rooms(names)
+
+    for badge in badges:
+        print(badge)
+
+    for assignment in room_assignments:
+        print(assignment)
     return None
